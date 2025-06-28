68421fae7aa9c209cd081d5da5ce564b391f4aac
NiayeshAbbaszade/Python_exercise <niayeshabbaszade.20@gmail.com>
2025-06-28 13:19:05 +0300

    Ping-pong

--- session one/ping_pong.py
+++ session one/ping_pong.py
@@ -2,29 +2,39 @@ import arcade
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 400
+SCREEN_TITLE = "Ping Pong "
 
-class PingPongGame(arcade.Window):
+PADDLE_WIDTH = 20
+PADDLE_HEIGHT = 80
+BALL_RADIUS = 10
+
+class SimplePingPong(arcade.Window):
     def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Ping Pong Game")
-        arcade.set_background_color(arcade.color.RED)
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        arcade.set_background_color(arcade.color.DARK_BLUE)
 
-        self.paddle_left = [50, SCREEN_HEIGHT // 2, 10, 100]  # x, y, width, height
-        self.paddle_right = [SCREEN_WIDTH - 50, SCREEN_HEIGHT // 2, 10, 100]
+        self.left_paddle_x = 30
+        self.left_paddle_y = SCREEN_HEIGHT // 2
 
-        self.ball = [SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, 10]  # x, y, radius
+        self.right_paddle_x = SCREEN_WIDTH - 30
+        self.right_paddle_y = SCREEN_HEIGHT // 2
+
+        self.ball_x = SCREEN_WIDTH // 2
+        self.ball_y = SCREEN_HEIGHT // 2
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_rectangle_filled(self.paddle_left[0], self.paddle_left[1], self.paddle_left[2], self.paddle_left[3], arcade.color.WHITE)
-        arcade.draw_rectangle_filled(self.paddle_right[0], self.paddle_right[1], self.paddle_right[2], self.paddle_right[3], arcade.color.WHITE)
-        arcade.draw_circle_filled(self.ball[0], self.ball[1], self.ball[2], arcade.color.WHITE)
 
-        arcade.draw_line(SCREEN_WIDTH // 2, 0, SCREEN_WIDTH // 2, SCREEN_HEIGHT, arcade.color.WHITE, 2)
+        arcade.draw_rect_filled(self.left_paddle_x, self.left_paddle_y,PADDLE_WIDTH, PADDLE_HEIGHT, arcade.color.WHITE)
 
-    def update(self, delta_time):
-       
-        pass
+        arcade.draw_rect_filled(self.right_paddle_x, self.right_paddle_y,PADDLE_WIDTH, PADDLE_HEIGHT, arcade.color.WHITE)
+
+        arcade.draw_circle_filled(self.ball_x, self.ball_y, BALL_RADIUS, arcade.color.WHITE)
+
+        arcade.draw_line(SCREEN_WIDTH // 2, 0, SCREEN_WIDTH // 2,SCREEN_HEIGHT, arcade.color.WHITE, 2)
+
+        arcade.finish_render()
 
 if __name__ == "__main__":
-    game = PingPongGame()
+    game = SimplePingPong()
     arcade.run()
